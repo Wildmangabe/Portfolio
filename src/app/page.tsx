68942e2163f5b2fe8e59f2ac8167570e826ded2c
Gabe Wild <wildmangabe@gmail.com>
2026-03-16 14:21:24 -0500

    Fix resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,6 @@ export default function Home() {
         <ExperienceSection />
         <ProjectsSection />
         <TechStackSection />
-        <LeadershipSection />
         <ContactSection />
         <QuoteSection />
       </main>
